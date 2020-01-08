6d8be33a6cc4da9510c01c9c631c99878f4c2d0a
Sanjana <sanjana.raheja@wavemaker.com>
2020-01-08 13:08:44 +0530

    Accepting MySql host, username, password from the env properties if present.
    Change-Id: I1cb4f1b9b0770ec227f76080cad69d83de7e9d12

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/util/DataServiceConstants.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/util/DataServiceConstants.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2013 - 2017 WaveMaker, Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -46,15 +46,24 @@ public class DataServiceConstants {
 
     public static final String WM_MY_SQL_CLOUD_PASSWORD_TOKEN = "{WM_CLOUD_MYSQL_PASSWORD}";
 
-    public static final String WM_MY_SQL_CLOUD_HOST = System.getProperty("wm.mysqlCloudHost", "localhost:3306");
+    public static final String WM_MY_SQL_CLOUD_HOST;
 
-    public static final String WM_MY_SQL_CLOUD_USER_NAME = System.getProperty("wm.mysqlCloudUsername", "root");
+    public static final String WM_MY_SQL_CLOUD_USER_NAME;
 
-    public static final String WM_MY_SQL_CLOUD_PASSWORD = System.getProperty("wm.mysqlCloudPassword", "cloudjee123");
+    public static final String WM_MY_SQL_CLOUD_PASSWORD;
 
     public static final String QUERY_EXECUTION_CONTROLLER = "QueryExecutionController";
 
     public static final String PROCEDURE_EXECUTION_CONTROLLER = "ProcedureExecutionController";
+
+    static {
+        final String mySqlCloudHost = "wm.mysqlCloudHost";
+        final String mySqlCloudUsername = "wm.mysqlCloudUsername";
+        final String mySqlCloudPassword = "wm.mysqlCloudPassword";
+        WM_MY_SQL_CLOUD_HOST = (System.getenv(mySqlCloudHost) != null) ? System.getenv(mySqlCloudHost) : System.getProperty(mySqlCloudHost, "localhost:3306");
+        WM_MY_SQL_CLOUD_USER_NAME = (System.getenv(mySqlCloudUsername) != null) ? System.getenv(mySqlCloudUsername) : System.getProperty(mySqlCloudUsername, "root");
+        WM_MY_SQL_CLOUD_PASSWORD = (System.getenv(mySqlCloudPassword) != null) ? System.getenv(mySqlCloudPassword) : System.getProperty(mySqlCloudPassword, "cloudjee123");
+    }
 
     private DataServiceConstants() {
     }
