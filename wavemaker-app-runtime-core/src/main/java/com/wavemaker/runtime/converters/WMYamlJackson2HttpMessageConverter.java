a87e253670501759a422bfa52de4ebae21cb2248
Akshay Kotagiri <akshay.kotagiri@wavemaker.com>
2020-04-01 11:14:06 +0530

    yaml support for swagger imports added WMYamlMessageConverter
    Change-Id: I515fdcd19fe4c2a9e9220b7709bacf664b041116

---
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/converters/WMYamlJackson2HttpMessageConverter.java
@@ -0,0 +1,43 @@
+package com.wavemaker.runtime.converters;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+public class WMYamlJackson2HttpMessageConverter extends WMCustomAbstractHttpMessageConverter<Map<String, Object>> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+
+    public WMYamlJackson2HttpMessageConverter() {
+        super(new MediaType("application", "x-yaml"), new MediaType("text", "x-yaml"),
+                new MediaType("application", "yml"), new MediaType("text", "yml"));
+    }
+
+    @Override
+    protected boolean supports(Class<?> clazz) {
+        return Map.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    protected Map<String, Object> readInternal(Class<? extends Map<String, Object>> clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+        return objectMapper.readValue(inputMessage.getBody(), Map.class);
+    }
+
+    @Override
+    protected void writeInternal(Map<String, Object> map, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+
+    }
+
+    @Override
+    public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+}
