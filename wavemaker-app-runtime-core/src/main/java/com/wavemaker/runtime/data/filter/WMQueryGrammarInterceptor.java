0a56e685a4fd71b18044f79233d0610baffd5951
Sujith <sujith.simon@wavemaker.com>
2020-07-24 15:20:35 +0530

    WMS-18884 Removing string escape characters from query parameter values.
    Change-Id: I1e8f04ca448dcc816467baafa173ac87f1b87701

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/filter/WMQueryGrammarInterceptor.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/filter/WMQueryGrammarInterceptor.java
@@ -1,5 +1,9 @@
 package com.wavemaker.runtime.data.filter;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import com.wavemaker.runtime.data.filter.parser.HqlFilterPropertyResolver;
 import com.wavemaker.runtime.data.filter.parser.HqlFilterPropertyResolverImpl;
 import com.wavemaker.runtime.data.filter.parser.HqlParser;
@@ -10,13 +14,32 @@ import com.wavemaker.runtime.data.filter.parser.HqlParser;
  */
 public class WMQueryGrammarInterceptor implements QueryInterceptor {
 
+    private static final Map<String, String> paramValueReplacements = new HashMap<>();
+
+    static {
+        paramValueReplacements.put("''", "'");
+    }
+
     @Override
     public void intercept(WMQueryInfo queryInfo, Class<?> entity) {
 
         HqlFilterPropertyResolver resolver = new HqlFilterPropertyResolverImpl(entity);
         WMQueryInfo parsedQuery = HqlParser.getInstance().parse(queryInfo.getQuery(), resolver);
 
+        removeEscapeCharactersFromParameterValues(parsedQuery);
+
         queryInfo.setQuery(parsedQuery.getQuery());
         queryInfo.getParameters().putAll(parsedQuery.getParameters());
+    }
+
+    private void removeEscapeCharactersFromParameterValues(WMQueryInfo wmQueryInfo) {
+        for (WMQueryParamInfo param : wmQueryInfo.getParameters().values()) {
+            for (Map.Entry<String, String> paramValueReplacement : paramValueReplacements.entrySet()) {
+                String paramValue = Objects.toString(param.getValue(), "");
+                if (paramValue.contains(paramValueReplacement.getKey())) {
+                    param.setValue(paramValue.replaceAll(paramValueReplacement.getKey(), paramValueReplacement.getValue()));
+                }
+            }
+        }
     }
 }
