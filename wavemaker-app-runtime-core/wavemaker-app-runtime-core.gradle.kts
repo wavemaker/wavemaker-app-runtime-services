c2b152dda3e470c1640277b35228e937c26d43c8
Uday Shankar <uday.adepu@wavemaker.com>
2020-08-29 15:01:40 +0530

    Updated patch versions of spring, spring-security and jackson libraries
    Change-Id: I96238d83ea4e94f229deb1d7fe19e2bd14f9a6e2

--- wavemaker-app-runtime-core/wavemaker-app-runtime-core.gradle.kts
+++ wavemaker-app-runtime-core/wavemaker-app-runtime-core.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     compileOnly("org.springframework.social:spring-social-web:1.1.6.RELEASE")
     //TODO Need to add xalan and xml-apis exclusions for saml2-core
     compileOnly("org.springframework.security.extensions:spring-security-saml2-core")
-    compileOnly("net.sf.jasperreports:jasperreports:6.13.0") {
+    compileOnly("net.sf.jasperreports:jasperreports:6.14.0") {
         //TODO need to revisit this
         exclude(group = "com.lowagie")
     }
