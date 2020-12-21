28dbcaa63f49bc5c27ccd6e2903ffb0aca6245df
akshay <akshay.kotagiri@wavemaker.com>
2020-12-21 15:02:51 +0530

    Added a FirewallFilter Which wraps request and response objects with StricthttpFirewall which will validate crlf.
    Change-Id: I00cbfd5ad8cf337da5fa993afe2edbcbd49908a0

---
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/filter/FirewallFilter.java
@@ -0,0 +1,41 @@
+package com.wavemaker.runtime.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.web.firewall.FirewalledRequest;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+public class FirewallFilter implements Filter {
+
+    private HttpFirewall firewall = new StrictHttpFirewall();
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        FirewalledRequest firewalledRequest = firewall.getFirewalledRequest((HttpServletRequest) request);
+        HttpServletResponse firewalledResponse = firewall.getFirewalledResponse((HttpServletResponse) response);
+
+        filterChain.doFilter(firewalledRequest, firewalledResponse);
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
+
