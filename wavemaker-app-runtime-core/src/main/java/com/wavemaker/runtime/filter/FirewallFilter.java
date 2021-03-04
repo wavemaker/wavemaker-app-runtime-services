8ab2f42f0c5dcd63d388587313c36fb45eb51290
akshay <akshay.kotagiri@wavemaker.com>
2021-03-04 12:40:06 +0530

    WMS-20154 Fix for veracode CWE ID : 601 and host header injection issue.

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/filter/FirewallFilter.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/filter/FirewallFilter.java
@@ -1,7 +1,10 @@
 package com.wavemaker.runtime.filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -11,19 +14,33 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.firewall.FirewalledRequest;
-import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 
 /*
-* This filter wraps ServletRequest and ServletResponse objects with StrictHttpFirewall
-* FirewalledResponse validates and blocks any CRLF characters if added to response object.
-* */
+ * This filter wraps ServletRequest and ServletResponse objects with StrictHttpFirewall
+ * FirewalledResponse validates and blocks any CRLF characters if added to response object.
+ * */
 
 public class FirewallFilter implements Filter {
 
-    private HttpFirewall firewall = new StrictHttpFirewall();
+    private StrictHttpFirewall firewall = new StrictHttpFirewall();
+
+    @Value("${app.request.allowedHosts}")
+    private String hosts;
+
+    @PostConstruct
+    private void init() {
+        firewall.setAllowedHostnames(hostName -> {
+            if (hosts != null && !hosts.isEmpty()) {
+                List<String> allowedHosts = Arrays.asList(hosts.split(","));
+                return allowedHosts.contains(hostName);
+            }
+            return true;
+        });
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -31,7 +48,6 @@ public class FirewallFilter implements Filter {
         HttpServletResponse firewalledResponse = firewall.getFirewalledResponse((HttpServletResponse) response);
 
         filterChain.doFilter(firewalledRequest, firewalledResponse);
-
     }
 
     @Override
