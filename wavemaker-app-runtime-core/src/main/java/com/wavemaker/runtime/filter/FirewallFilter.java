861203fd5c5d4ba14a2d95e981ced1eb6bcb0955
akshay <akshay.kotagiri@wavemaker.com>
2021-04-01 13:36:19 +0530

    WMS-20236 Refactoring FirewallFilter added logger statement

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/filter/FirewallFilter.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/filter/FirewallFilter.java
@@ -14,6 +14,9 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.firewall.FirewalledRequest;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
@@ -28,15 +31,21 @@ public class FirewallFilter implements Filter {
 
     private StrictHttpFirewall firewall = new StrictHttpFirewall();
 
+    private static final Logger logger = LoggerFactory.getLogger(FirewallFilter.class);
+
     @Value("${general.request.allowedHosts}")
     private String hosts;
 
+    private List<String> allowedHosts;
+
     @PostConstruct
     private void init() {
+        logger.info("Allowed hostnames configured are {}", hosts);
+        String hostNamesLower = StringUtils.lowerCase(hosts);
+        allowedHosts = Arrays.asList(hostNamesLower.split(","));
         firewall.setAllowedHostnames(hostName -> {
             if (hosts != null && !hosts.isEmpty()) {
-                List<String> allowedHosts = Arrays.asList(hosts.split(","));
-                return allowedHosts.contains(hostName);
+                return allowedHosts.contains(StringUtils.lowerCase(hostName));
             }
             return true;
         });
