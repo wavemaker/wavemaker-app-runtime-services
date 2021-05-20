af67b13dd90d0e0c08de73f91a134edc673d68c2
praveen_chandra <praveen.pokuri@wavemaker.com>
2021-05-20 15:07:23 +0530

    WMS-20605 Fixed NPE in weblogic server after application deployment

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/filter/compression/WMCompressionFilter.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/filter/compression/WMCompressionFilter.java
@@ -15,8 +15,8 @@
  */
 package com.wavemaker.runtime.filter.compression;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
@@ -60,9 +60,8 @@ public class WMCompressionFilter extends GenericFilterBean {
             for (String encodingType : BUILD_TIME_ENCODINGS) {
                 if (supportedEncodings.toLowerCase().contains(encodingType)) {
                     String generatedCompressedFile = accessingResource.replaceAll("(.*)(\\..*)", "$1." + encodingType + "$2");
-                    File compressedFile = new File(getServletContext().getRealPath(
-                            FileValidationUtils.validateFilePath(generatedCompressedFile)));
-                    if (compressedFile.exists() && compressedFile.isFile()) {
+                    URL compressedResource = getServletContext().getResource(FileValidationUtils.validateFilePath(generatedCompressedFile));
+                    if (compressedResource != null && accessingResource.matches("(.*)(\\..*)")) {
                         processedCompressedFile = true;
                         RequestDispatcher requestDispatcher = httpServletRequest.getRequestDispatcher("/" + generatedCompressedFile);
                         httpServletResponse.addHeader(HttpHeaders.CONTENT_ENCODING, encodingType);
