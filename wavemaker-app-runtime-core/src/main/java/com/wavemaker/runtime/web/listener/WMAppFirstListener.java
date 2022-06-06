f10c16d7934aa26c8f2830558fb96022274c898a
Praveen Chandra <praveen.pokuri@wavemaker.com>
2022-06-06 15:47:16 +0530

    WMS-22991 Fixed log4j memory leak issue.

---
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/web/listener/WMAppFirstListener.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2020 WaveMaker, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wavemaker.runtime.web.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.slf4j.MDC;
+
+import com.wavemaker.runtime.web.filter.WMRequestFilter;
+
+/**
+ * This is the first listener declared in the web.xml.
+ * It sets the mdc value in its contextInitialized method.
+ *
+ * This works in conjunction with {@link WMAppLastListener} class which is the last listener defined in the web application.
+ *
+ * Its contextDestroyed method is the last method to be called during un deployment, and it removes the mdc value which is set in contextDestroyed method of
+ * {@link WMAppLastListener} class
+ *
+ * @author Uday Shankar
+ */
+public class WMAppFirstListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        MDC.put(WMRequestFilter.APP_NAME_KEY, sce.getServletContext().getContextPath());
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        MDC.remove(WMRequestFilter.APP_NAME_KEY);
+    }
+}
---
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/web/listener/WMAppLastListener.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2020 WaveMaker, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wavemaker.runtime.web.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.slf4j.MDC;
+
+import com.wavemaker.runtime.web.filter.WMRequestFilter;
+
+/**
+ * This is the last listener declared in the web.xml.
+ *
+ * It removes the mdc value set by {@link WMAppFirstListener} in its contextInitialized method as it will be the last method to be called.
+ *
+ * This works in conjunction with {@link WMAppFirstListener} class which is the first listener defined in the web application.
+ *
+ * Its contextDestroyed method is the first to be called during un deployment, and it sets the mdc value which is removed in contextDestroyed method of
+ * {@link WMAppFirstListener} class
+ *
+ * @author Uday Shankar
+ */
+public class WMAppLastListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        MDC.remove(WMRequestFilter.APP_NAME_KEY);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        MDC.put(WMRequestFilter.APP_NAME_KEY, sce.getServletContext().getContextPath());
+    }
+}
