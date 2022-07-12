293045a7a903f94527271f144b59c3cecdee0ca5
Praveen Chandra <praveen.pokuri@wavemaker.com>
2022-07-12 15:03:21 +0530

    WMS-22585 Replace cdnUrl placeholder in index.html during runtime using filter

---
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/web/filter/HtmlFilter.java
@@ -0,0 +1,19 @@
+package com.wavemaker.runtime.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+public class HtmlFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+}
