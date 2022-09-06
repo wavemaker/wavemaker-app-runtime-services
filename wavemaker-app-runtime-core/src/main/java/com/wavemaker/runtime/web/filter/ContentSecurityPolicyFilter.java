35328a6b75c0fe5df630a1cabbeb7aa1b664831f
Amair Khan <mohammed.khan@wavemaker.com>
2022-09-06 15:28:48 +0530

    WMS-22905 - using antPathRequestMatcher for checking the request path

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/web/filter/ContentSecurityPolicyFilter.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/web/filter/ContentSecurityPolicyFilter.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.filter.GenericFilterBean;
 
 public class ContentSecurityPolicyFilter extends GenericFilterBean {
@@ -30,6 +31,8 @@ public class ContentSecurityPolicyFilter extends GenericFilterBean {
     private static final String NONCE_PLACEHOLDER = "${NONCE_VALUE}";
     private static final String CSP_HEADER = "Content-Security-Policy";
     private static final Logger logger = LoggerFactory.getLogger(ContentSecurityPolicyFilter.class);
+
+    private AntPathRequestMatcher requestMatcher = new AntPathRequestMatcher("/index.html");
 
     @Override
     protected void initFilterBean() {
@@ -67,7 +70,7 @@ public class ContentSecurityPolicyFilter extends GenericFilterBean {
     }
 
     private boolean requestMatches(HttpServletRequest httpServletRequest) {
-        return httpServletRequest.getServletPath().equals("/") || httpServletRequest.getServletPath().equals("/index.html");
+        return this.requestMatcher.matches(httpServletRequest);
     }
 
     private String generateRandomNonce(int length) {
