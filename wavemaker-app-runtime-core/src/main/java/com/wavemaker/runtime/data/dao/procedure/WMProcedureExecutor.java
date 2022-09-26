5c85dce6cd552475781dd8d20c63b235139965df
Praveen Chandra <praveen.pokuri@wavemaker.com>
2022-09-26 12:17:46 +0530

    WMS-22961 Reverted executeNamedProcedure method
    (cherry picked from commit b27dde67d698410d1f450f1e2205d3770db682de)

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/dao/procedure/WMProcedureExecutor.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/dao/procedure/WMProcedureExecutor.java
@@ -15,9 +15,13 @@
  */
 package com.wavemaker.runtime.data.dao.procedure;
 
+import java.util.List;
 import java.util.Map;
 
 public interface WMProcedureExecutor {
 
     <T> T executeNamedProcedure(String procedureName, Map<String, Object> params, Class<T> type);
+
+    @Deprecated
+    List<Object> executeNamedProcedure(String procedureName, Map<String, Object> params);
 }
