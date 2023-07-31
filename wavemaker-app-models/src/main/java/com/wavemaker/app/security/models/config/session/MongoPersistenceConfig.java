d8482bb74da177c3853354bbf729687b68fd275b
Uday Shankar <uday.adepu@wavemaker.com>
2023-07-31 07:22:14 +0530

    WMS-24080 Updated copyrightlicense headers

--- wavemaker-app-models/src/main/java/com/wavemaker/app/security/models/config/session/MongoPersistenceConfig.java
+++ wavemaker-app-models/src/main/java/com/wavemaker/app/security/models/config/session/MongoPersistenceConfig.java
@@ -1,9 +1,16 @@
 /*******************************************************************************
- * Copyright (C) 2022-2023 WaveMaker, Inc. All Rights Reserved.
+ * Copyright (C) 2022-2023 WaveMaker, Inc.
  *
- * This software is the confidential and proprietary information of WaveMaker, Inc.
- * You shall not disclose such Confidential Information and shall use it only in accordance with the
- * terms of the source code license agreement you entered into with WaveMaker, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ******************************************************************************/
 
 package com.wavemaker.app.security.models.config.session;
