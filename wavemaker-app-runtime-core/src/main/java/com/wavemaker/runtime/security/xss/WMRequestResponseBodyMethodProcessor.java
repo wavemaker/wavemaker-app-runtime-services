dd450a0935e0964d1d42876096c10f0c64f72d76
Praveen Chandra <praveen.pokuri@wavemaker.com>
2024-05-21 17:24:22 +0530

    WMS-25749 Refactored RecursiveObjectProcessor to handle list, map, set, arrays and custom objects and also XSS to use RecursiveObjectProcessor

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/xss/WMRequestResponseBodyMethodProcessor.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/xss/WMRequestResponseBodyMethodProcessor.java
@@ -15,34 +15,31 @@
 
 package com.wavemaker.runtime.security.xss;
 
-import java.awt.print.Pageable;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotWritableException;
-import org.springframework.util.ReflectionUtils;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
-import com.wavemaker.commons.MessageResource;
-import com.wavemaker.commons.WMRuntimeException;
-import com.wavemaker.runtime.security.xss.handler.XSSSecurityHandler;
+import com.wavemaker.commons.processor.ChildObjectRetriever;
+import com.wavemaker.commons.processor.DefaultChildObjectRetriever;
+import com.wavemaker.commons.processor.ObjectProcessor;
+import com.wavemaker.commons.processor.ProcessContext;
+import com.wavemaker.commons.processor.RecursiveObjectProcessor;
 
 public class WMRequestResponseBodyMethodProcessor extends RequestResponseBodyMethodProcessor {
 
@@ -56,17 +53,13 @@ public class WMRequestResponseBodyMethodProcessor extends RequestResponseBodyMet
     @Override
     public void handleReturnValue(Object returnValue, MethodParameter returnType, ModelAndViewContainer mavContainer, NativeWebRequest webRequest) throws IOException, HttpMediaTypeNotAcceptableException, HttpMessageNotWritableException {
         Method method = returnType.getMethod();
-        boolean xssDisable;
 
-        xssDisable = Arrays.stream(Objects.requireNonNull(method).getDeclaringClass().getDeclaredAnnotations())
-            .anyMatch(an -> an instanceof XssDisable) || Arrays.stream(method.getDeclaredAnnotations())
-            .anyMatch(an -> an instanceof XssDisable);
+        boolean xssDisable = Arrays.stream(Objects.requireNonNull(method).getDeclaringClass().getDeclaredAnnotations())
+            .anyMatch(XssDisable.class::isInstance) || Arrays.stream(method.getDeclaredAnnotations())
+            .anyMatch(XssDisable.class::isInstance);
 
         if (!xssDisable) {
-            ResponseTuple encodeResult = encode(returnValue, new ArrayList<>(), DataFlowType.OUTGOING);
-            if (encodeResult.modified) {
-                returnValue = encodeResult.value;
-            }
+            returnValue = getSanitizedObject(returnValue, DataFlowType.OUTGOING);
         }
         super.handleReturnValue(returnValue, returnType, mavContainer, webRequest);
     }
@@ -74,133 +67,28 @@ public class WMRequestResponseBodyMethodProcessor extends RequestResponseBodyMet
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         Object o = super.resolveArgument(parameter, mavContainer, webRequest, binderFactory);
-        boolean xssDisable;
-        xssDisable = Arrays.stream(Objects.requireNonNull(parameter.getMethod()).getDeclaringClass().getDeclaredAnnotations())
-            .anyMatch(an -> an instanceof XssDisable) || Arrays.stream(parameter.getMethod().getDeclaredAnnotations())
-            .anyMatch(an -> an instanceof XssDisable);
+        boolean xssDisable = Arrays.stream(Objects.requireNonNull(parameter.getMethod()).getDeclaringClass().getDeclaredAnnotations())
+            .anyMatch(XssDisable.class::isInstance) || Arrays.stream(Objects.requireNonNull(parameter.getMethod()).getDeclaredAnnotations())
+            .anyMatch(XssDisable.class::isInstance);
         if (!xssDisable) {
-            ResponseTuple responseTuple = encode(o, new ArrayList(), DataFlowType.INCOMING);
-            return responseTuple.value;
+            return getSanitizedObject(o, DataFlowType.INCOMING);
         }
         return o;
     }
 
-    private ResponseTuple encode(Object value, List<Object> manipulatedObjects, DataFlowType dataFlowType) {
-        Object encoded = value;
-        boolean modified = false;
-
-        if (value != null && !(value instanceof Number) && !(containsInList(value, manipulatedObjects) || isExcludedClass(value))) {
-            final Class<?> valueClass = value.getClass();
-
-            if (valueClass == char[].class) {
-                encoded = encode(((char[]) value), dataFlowType);
-                modified = true;
-            } else if (valueClass.isArray()) {
-                modified = encodeArray((Object[]) value, manipulatedObjects, dataFlowType);
-            } else if (value instanceof String) {
-                encoded = encode((String) value, dataFlowType);
-                modified = true;
-            } else {
-                manipulatedObjects.add(value);
-                final ResponseTuple response = encodeCustomClass(value, manipulatedObjects, dataFlowType);
-                if (response.modified) {
-                    encoded = response.value;
-                    modified = true;
-                }
-            }
-        }
-
-        return new ResponseTuple(encoded, modified);
+    private Object getSanitizedObject(Object object, DataFlowType dataFlowType) {
+        ChildObjectRetriever childObjectRetriever = new DefaultChildObjectRetriever(this::isXssEnabledForField);
+        ObjectProcessor xssEncodeProcessor = new XssEncodeProcessor(dataFlowType);
+        RecursiveObjectProcessor recursiveObjectProcessor =
+            new RecursiveObjectProcessor(xssEncodeProcessor, childObjectRetriever, new ProcessContext(), this::isXssEnabled);
+        return recursiveObjectProcessor.processRootObject(object);
     }
 
-    private boolean encodeArray(Object[] valueArray, final List<Object> manipulatedObjects, DataFlowType dataFlowType) {
-        boolean modified = false;
-        for (int i = 0; i < valueArray.length; i++) {
-            Object obj = valueArray[i];
-            ResponseTuple result = encode(obj, manipulatedObjects, dataFlowType);
-            if (result.modified) {
-                valueArray[i] = result.value;
-                modified = true;
-            }
-        }
-        return modified;
+    private boolean isXssEnabled(Object object) {
+        return EXCLUDED_CLASSES.stream().noneMatch(type -> type.isInstance(object)) && !object.getClass().isAnnotationPresent(XssDisable.class);
     }
 
-    private ResponseTuple encodeCustomClass(Object object, List<Object> manipulatedObjects, DataFlowType dataFlowType) {
-        AtomicBoolean modified = new AtomicBoolean(false);
-        if (object instanceof Page) {
-            ((Page) object).getContent().forEach(obj -> {
-                obj = encode(obj, manipulatedObjects, dataFlowType).value;
-                modified.set(true);
-            });
-        }
-        for (final Field field : object.getClass().getDeclaredFields()) {
-            if (!field.isAnnotationPresent(XssDisable.class) && !Modifier.isFinal(field.getModifiers())) {
-                ReflectionUtils.makeAccessible(field);
-                try {
-                    final ResponseTuple response = encode(field.get(object), manipulatedObjects, dataFlowType);
-                    if (response.modified) {
-                        modified.set(true);
-                        field.set(object, response.value);
-                    }
-                } catch (IllegalAccessException e) {
-                    throw new WMRuntimeException(MessageResource.create("reflection.field.error"), e, field.getName(),
-                        field.getDeclaringClass().getName());
-                }
-            }
-        }
-        return new ResponseTuple(object, modified.get());
-    }
-
-    private char[] encode(char[] value, DataFlowType dataFlowType) {
-        return encode(new String(value), dataFlowType).toCharArray();
-    }
-
-    private String encode(String value, DataFlowType dataFlowType) {
-        switch (dataFlowType) {
-            case OUTGOING:
-                return XSSSecurityHandler.getInstance().sanitizeOutgoingData(value);
-            case INCOMING:
-                return XSSSecurityHandler.getInstance().sanitizeIncomingData(value);
-            default:
-                throw new IllegalStateException();
-        }
-    }
-
-    private boolean isExcludedClass(Object value) {
-        Class<?> valueClass = value.getClass();
-        boolean excluded = valueClass.isAnnotationPresent(XssDisable.class);
-        if (!excluded) {
-            excluded = (valueClass.getComponentType() != null && valueClass.getComponentType().isPrimitive() && valueClass != char[].class);
-
-            if (!excluded) {
-                excluded = EXCLUDED_CLASSES.stream().anyMatch(type -> type.isInstance(value));
-            }
-        }
-        return excluded;
-    }
-
-    private boolean containsInList(Object value, List<Object> manipulatedObjects) {
-        for (Object obj : manipulatedObjects) {
-            if (obj == value) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static class ResponseTuple {
-        private Object value;
-        private boolean modified;
-
-        public ResponseTuple(final Object value, final boolean modified) {
-            this.value = value;
-            this.modified = modified;
-        }
-    }
-
-    private enum DataFlowType {
-        INCOMING,
-        OUTGOING
+    private boolean isXssEnabledForField(Field field) {
+        return !field.isAnnotationPresent(XssDisable.class);
     }
 }
