b35da08aad14045512de4e43f97befc508a3776e
Abhitej25 <abhitej.konkimalla@wavemaker.com>
2025-02-10 12:48:12 +0530

    WMS-27527 added a new property to take the list of apis that can be ignored while csrf protection.

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/csrf/CsrfSecurityRequestMatcher.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/csrf/CsrfSecurityRequestMatcher.java
@@ -15,11 +15,17 @@
 package com.wavemaker.runtime.security.csrf;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -33,17 +39,25 @@ import com.wavemaker.app.security.models.CSRFConfig;
  */
 public class CsrfSecurityRequestMatcher implements RequestMatcher {
 
-    private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTION)$");
+    @Autowired
+    private Environment environment;
+
+    private Set<String> unprotectedHttpMethods;
     private List<RegexRequestMatcher> unprotectedMatchers;
 
     private CSRFConfig csrfConfig;
 
-    public CsrfSecurityRequestMatcher() {
-    }
-
-    public CsrfSecurityRequestMatcher(List<String> patterns) {
-        unprotectedMatchers = new ArrayList<>(patterns.size());
-        for (String pattern : patterns) {
+    @PostConstruct
+    public void init() {
+        String unprotectedMatchersStr = environment.getProperty("security.general.xsrf.unprotectedUrlMatchers");
+        String unprotectedMethodsStr = environment.getProperty("security.general.xsrf.unprotectedHttpMethods", "GET,HEAD,TRACE,OPTIONS");
+        List<String> unprotectedMatchersList = new ArrayList<>();
+        if (StringUtils.isNotBlank(unprotectedMatchersStr)) {
+            unprotectedMatchersList = Arrays.stream(unprotectedMatchersStr.split(",")).toList();
+        }
+        unprotectedHttpMethods = Arrays.stream(unprotectedMethodsStr.split(",")).collect(Collectors.toSet());
+        unprotectedMatchers = new ArrayList<>(unprotectedMatchersList.size());
+        for (String pattern : unprotectedMatchersList) {
             unprotectedMatchers.add(new RegexRequestMatcher(pattern, null));
         }
     }
@@ -57,7 +71,7 @@ public class CsrfSecurityRequestMatcher implements RequestMatcher {
         if (csrfConfig == null || !csrfConfig.isEnforceCsrfSecurity()) {
             return false;
         }
-        if (allowedMethods.matcher(httpServletRequest.getMethod()).matches()) {
+        if (unprotectedHttpMethods.contains(httpServletRequest.getMethod())) {
             return false;
         }
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
